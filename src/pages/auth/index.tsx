be86a9b28325c7ec6ae2f199342adda448b4ea94
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-05-18 10:07:28 +0700

    add hero

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -8,6 +8,9 @@ const Auth: FC<AuthProps> = ({}) => {
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
         <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <img src="/hero.png" alt="hero img" />
+        </div>
       </div>
     </div>
   );
